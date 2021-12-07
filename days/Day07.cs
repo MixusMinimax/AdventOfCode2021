f83d1e3f610e757e60e6501d9704e4869853ea90
MixusMinimax <maxi.barmetler@gmail.com>
2021-12-07 14:01:59 +0100

    Day 7

---
+++ days/Day07.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using advent_of_code.util;
+
+namespace advent_of_code.days;
+
+public class Day07 : IDay
+{
+    public IList<Func<string[], Task>> Steps { get; }
+
+    public Day07()
+    {
+        Steps = Common.CreateFileSteps(StepOne, StepTwo);
+    }
+
+    private static int GetCost(IEnumerable<int> numbers, int target)
+    {
+        return numbers.Select(e => Math.Abs(e - target)).Sum();
+    }
+
+    private static int[]? _costCache;
+
+    private static int GetCost(int steps)
+    {
+        int Cost(int s) => s == 0 ? 0 : s + Cost(s - 1);
+        _costCache ??= Enumerable.Range(0, 128).Select(Cost).ToArray();
+        return steps < _costCache.Length ? _costCache[steps] : Cost(steps);
+    }
+    
+    private static int GetCostAlternative(IEnumerable<int> numbers, int target)
+    {
+        return numbers.Select(e => GetCost(Math.Abs(e - target))).Sum();
+    }
+    
+    private static void StepOne(string path)
+    {
+        var numbers = File.ReadLines(path).FirstOrDefault("").SplitNumbers().ToList();
+        numbers.Sort();
+        var median = numbers.Count % 2 == 1
+            ? numbers[numbers.Count / 2]
+            : (numbers[numbers.Count / 2 - 1] + numbers[numbers.Count / 2]) / 2;
+        
+        Console.WriteLine($"Cost at target={median}: {GetCost(numbers, median)}");
+    }
+    
+    private static void StepTwo(string path)
+    {
+        var numbers = File.ReadLines(path).FirstOrDefault("").SplitNumbers().ToList();
+
+        var min = numbers.Min();
+        var max = numbers.Max();
+        
+        var bestTarget = Enumerable.Range(min, max - min + 1).MinBy(e => GetCostAlternative(numbers, e));
+        
+        Console.WriteLine($"Cost at target={bestTarget}: {GetCostAlternative(numbers, bestTarget)}");
+    }
+}
--- util/Extensions.cs
+++ util/Extensions.cs
@@ -9,7 +9,7 @@ public static class Extensions
         return bits
             .Aggregate(0, (a, b) => (a << 1) | (b ? 1 : 0));
     }
-    
+
     public static IEnumerable<TResult> SelectWhere<TSource, TResult>(
         this IEnumerable<TSource> enumerable,
         SelectWhereDelegate<TSource, TResult> func)
@@ -23,6 +23,11 @@ public static class Extensions
     public static IEnumerable<int> TryParseInt(this IEnumerable<string> enumerable)
     {
         return enumerable.SelectWhere<string, int>(int.TryParse);
+    }
+
+    public static IEnumerable<int> SplitNumbers(this string line, char delimiter = ',')
+    {
+        return line.Split(delimiter).TryParseInt();
     }
 
     public delegate bool SelectWhereDelegate<in TFrom, TTo>(TFrom from, out TTo to);
